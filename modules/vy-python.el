3fdb197ab57c029e00057c94a46f6f7c8048a2ab
jpegea <joaquimpascualegea@gmail.com>
2022-08-26 22:32:47 +0200

    Now using pyright for Python LSP. Many people feel it faster and maintened by Microsoft.

---
+++ modules/vy-python.el
@@ -0,0 +1,42 @@
+;; -*- lexical-binding: t; -*-
+
+(use-package lsp-mode
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :config
+  (lsp-enable-which-key-integration t)
+  :hook (python-mode . lsp)
+  :custom
+  (lsp-headerline-breadcrumb-enable nil))
+
+(use-package lsp-ui
+  :after lsp-mode
+  :hook (lsp-mode . lsp-ui-mode)
+  :config
+  (lsp-ui-doc-enable t)
+  ;; :custom
+  ;; (lsp-ui-doc-position 'bottom)
+  )
+
+(use-package lsp-treemacs
+  :after (lsp-mode treemacs))
+
+(use-package company
+  :after lsp-mode
+  :hook (lsp-mode . company-mode)
+  :bind
+  (:map company-active-map
+	("<tab>" . company-complete-selection))
+  (:map lsp-mode-map
+	("<tab>" . company-indent-or-complete-common))
+  :custom
+  (company-minimum-prefix-length 1)
+  (company-idle-delay 0.0))
+
+(use-package yasnippet
+  :config (yas-reload-all)
+  :hook (python-mode . yas-minor-mode))
+
+(use-package lsp-pyright)
+
+(provide 'vy-python)
