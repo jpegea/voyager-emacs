b28b9bfa51c2928a483daefbd951c60fe0810c84
jpegea <jopase2@alumni.uv.es>
2022-08-30 15:02:12 +0200

    Add an example of personal config.

---
+++ example_voyager_config/early-init.el
@@ -0,0 +1,8 @@
+(require 'vy-defaults)
+(require 'vy-ui)
+(require 'vy-completion)
+(require 'vy-latex)
+(require 'vy-evil)
+(require 'vy-python)
+
+(set-face-attribute 'default nil :font "Fira Code Retina" :height 105)
