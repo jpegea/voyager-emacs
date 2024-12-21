980496a3e27d8b17ffcc7e3bf37b661007b45e78
jpegea <joaquimpascualegea@gmail.com>
2024-12-21 13:31:23 +0100

    new defaults for lang-latex

--- modules/lang-latex.el
+++ modules/lang-latex.el
@@ -9,36 +9,44 @@
   (TeX-master nil)
   (TeX-debug-bad-boxes t)
   (TeX-debug-warnings t)
-  ;; (LaTeX-indent-level 4)
+  ; (LaTeX-indent-level 4)
+  (LaTeX-verbatim-environments '("verbatim" "verbatim*" "Verbatim" "Verbatim*" "filecontents" "filecontents*" "lstlisting"))
 
   :hook
-  ;; (LaTeX-mode . turn-on-auto-fill)
+  (LaTeX-mode . turn-on-auto-fill)
   (LaTeX-mode . turn-on-reftex)
   (LaTeX-mode . yas-minor-mode-on)
   (LaTeX-mode . company-mode)
   (LaTeX-mode . display-line-numbers-mode)
   (LaTeX-mode . toggle-word-wrap)
-  ;;(LaTeX-mode . hl-line-mode)
+  ;; (LaTeX-mode . hl-line-mode)
 
   :bind (:map LaTeX-mode-map
 	      ("C-<return>" . TeX-save-compile-and-clean)
 	      ("C-c M-q" . LaTeX-fill-paragraph)
 	      ("C-c C-S-e" . LaTeX-change-environment)
-	      ("C-c i" . indent-region)
-	      ("C-c C-f b" . LaTeX-find-matching-begin)
-	      ("C-c C-f e" . LaTeX-find-matching-end))
+	      ("C-c i" . indent-region))
 
   :config
-  (defun TeX-save-compile-and-clean ()
+  (defun TeX-save-compile-and-clean () ;; Falta acabar
     (interactive)
     (save-buffer)
     (TeX-command "LaTeX" 'TeX-master-file))
-
   (company-auctex-init))
 
 
 (use-package company-auctex
   :defer t
   :config (company-auctex-init))
+
+;; Testing
+;; (use-package lsp-ltex
+;;   :ensure t
+;;   :hook (LaTeX-mode . (lambda ()
+;; 						(require 'lsp-ltex)
+;; 						(lsp)))
+;;   :init
+;;   (setq lsp-ltex-version "16.0.0")
+;;   (setq-default lsp-ltex-language "ca-ES"))
 
 (provide 'lang-latex)
